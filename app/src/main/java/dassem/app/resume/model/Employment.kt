1348435aed5b28f345d065813a70c2854ded597f
dassem-ultor <dassem.ultor@o2.pl>
2019-09-21 16:02:05 +0200

    Add id to Employment model

--- app/src/main/java/dassem/app/resume/model/Employment.kt
+++ app/src/main/java/dassem/app/resume/model/Employment.kt
@@ -1,11 +1,13 @@
 package dassem.app.resume.model
 
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
-data class Employment (
-
-	@SerializedName("dates") val dates : String,
-	@SerializedName("company") val company : String,
-	@SerializedName("job_title") val jobTitle : String,
-	@SerializedName("responsibilities") val responsibilities : String
-)
+data class Employment(
+    @SerializedName("dates") val dates: String,
+    @SerializedName("company") val company: String,
+    @SerializedName("job_title") val jobTitle: String,
+    @SerializedName("responsibilities") val responsibilities: String
+) {
+    val id: UUID = UUID.randomUUID()
+}
