1a7e79cd6f53500641db3e83e6bb9687f26a85ee
dassem-ultor <dassem.ultor@o2.pl>
2019-09-21 16:26:00 +0200

    Remove toolbar from activity

--- app/src/main/java/dassem/app/resume/ui/MainActivity.kt
+++ app/src/main/java/dassem/app/resume/ui/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : DaggerAppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this, modelFactory)[MainViewModel::class.java]
         setContentView(R.layout.activity_scrolling)
-//        setSupportActionBar(toolbar)
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
