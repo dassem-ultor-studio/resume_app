2deb1cd11cd0448b6c9ff75bb375039b22f41a75
dassem-ultor <dassem.ultor@o2.pl>
2019-09-21 10:48:00 +0200

    Rename activity

--- app/src/main/java/dassem/app/resume/di/modules/ContributeModule.kt
+++ app/src/main/java/dassem/app/resume/di/modules/ContributeModule.kt
@@ -2,11 +2,11 @@ package dassem.app.resume.di.modules
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import dassem.app.resume.ui.ScrollingActivity
+import dassem.app.resume.ui.MainActivity
 
 @Module
 abstract class ContributeModule {
 
     @ContributesAndroidInjector
-    abstract fun contributeScrollingActivity(): ScrollingActivity
+    abstract fun contributeScrollingActivity(): MainActivity
 }
--- app/src/main/java/dassem/app/resume/ui/MainActivity.kt
+++ app/src/main/java/dassem/app/resume/ui/MainActivity.kt
@@ -5,19 +5,40 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import dagger.android.support.DaggerAppCompatActivity
 import dassem.app.resume.R
+import dassem.app.resume.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_scrolling.*
+import javax.inject.Inject
 
-class ScrollingActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var modelFactory: ViewModelProvider.Factory
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this, modelFactory)[MainViewModel::class.java]
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        viewModel.resume.observe(this, Observer {
+            Toast.makeText(this, "${it.education}", Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.errorMessage.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
