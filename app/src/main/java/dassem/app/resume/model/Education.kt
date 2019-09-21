052dd48ce290bc90fb72b710cde75e9fabdbdc82
dassem-ultor <dassem.ultor@o2.pl>
2019-09-21 16:23:53 +0200

    Update Education model

--- app/src/main/java/dassem/app/resume/model/Education.kt
+++ app/src/main/java/dassem/app/resume/model/Education.kt
@@ -2,9 +2,9 @@ package dassem.app.resume.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Education (
-	@SerializedName("dates") val dates : String,
-	@SerializedName("university") val university : String,
-	@SerializedName("key_modules") val keyModules : String,
-	@SerializedName("dissertation_topic") val dissertationTopic : String
+data class Education(
+    @SerializedName("dates") val dates: String,
+    @SerializedName("university") val university: String,
+    @SerializedName("course") val course: String,
+    @SerializedName("key_modules") val keyModules: String
 )
