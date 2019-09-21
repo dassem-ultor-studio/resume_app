0618f93ea6548aa437bb127faee5a07e9e4dfe70
dassem-ultor <dassem.ultor@o2.pl>
2019-09-21 10:52:50 +0200

    Add getResume method to service

--- app/src/main/java/dassem/app/resume/network/ResumeService.kt
+++ app/src/main/java/dassem/app/resume/network/ResumeService.kt
@@ -1,3 +1,11 @@
 package dassem.app.resume.network
 
-interface ResumeService
+import dassem.app.resume.model.Resume
+import io.reactivex.Single
+import retrofit2.http.GET
+
+
+interface ResumeService {
+    @GET("bins/6unz9")
+    fun getResume(): Single<Resume>
+}
