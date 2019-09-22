adfd2b77b83d3330badfd12d1ae556b046989ce8
dassem-ultor <dassem.ultor@o2.pl>
2019-09-22 13:21:33 +0200

    Create classes that help with testing livedata and rx

---
+++ app/src/test/java/dassem/app/resume/viewmodel/TestSchedulerExtension.kt
@@ -0,0 +1,23 @@
+package dassem.app.resume.viewmodel
+
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class TestSchedulerExtension : BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    override fun beforeTestExecution(context: ExtensionContext?) {
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setNewThreadSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setSingleSchedulerHandler { Schedulers.trampoline() }
+        RxAndroidPlugins.setMainThreadSchedulerHandler { Schedulers.trampoline() }
+    }
+
+    override fun afterTestExecution(context: ExtensionContext?) {
+        RxJavaPlugins.reset()
+        RxAndroidPlugins.reset()
+    }
+}
